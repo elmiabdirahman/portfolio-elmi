b37f0402ec4ea0771a6ad2b6c5eafe26e66c243c
Elmi Elmi <elmielmi522@gmail.com>
2021-01-07 01:35:27 -0600

    edit divs

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -27,16 +27,17 @@ class Sidebar extends Component {
                     <a href="https://www.facebook.com/elmiabdirahman.elmi"><img src={facebook} alt="facebook" className="sidebar__icon mr-3"></img></a>
                     <a href="https://www.linkedin.com/in/elmi-elmi-07182b186/"><img src={linkedin} alt="instagram" className="sidebar__icon"></img></a>
                 </figure>
-                <div className="sidebar__contact">
+                    <div className="sidebar__contact">
                     <div className="sidebar__item sidebar__github">
                         <a href="https://github.com/elmiabdirahman"><img src={github} alt="github" className="sidebar__icon mr-3"/>github</a>
                     </div>
                     <div className="sidebar__location">
                         <img src={pin} alt="location" className="sidebar__icon mr-3"/>
-                        Saint Paul Minnesota</div>
-                        <div className="sidebar__item">  elmielmi522@gmail.com </div>
+                        Saint Paul Minnesota
+                        </div>
+                        <div className="sidebar__item">elmielmi522@gmail.com </div>
                         <div className="sidebar__item">12323123/123123213</div>
-                </div>
+                    </div>
                 <div className="sidebar__item sidebar__email" onClick={handleEmailMe}>email me</div>
             </div>
         );
