c306d101b74e07c065986222246924c1928920f0
Elmi Elmi <elmielmi522@gmail.com>
2021-01-10 22:58:07 -0600

    added text in about

--- src/components/About.js
+++ src/components/About.js
@@ -4,6 +4,13 @@ const About = () => {
     return (
         <div>
             <h1>This is About</h1>
+            <div className="about">
+            <h6 className="about__intro">
+            I’m a full stack software engineer who has experience in creating websites and apps. 
+            I'm passionate about solving technical problems using the full stack methodology and 
+            popular industry languages.
+            </h6>
+            </div>
         </div>
     );
 };
